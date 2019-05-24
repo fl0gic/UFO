ca6da415f958808382358459b04ec2bc652a21fc
flogic <GamerKing195@gmail.com>
2019-05-24 11:50:08 -0600

    Initial commit.

---
+++ Assets/Scripts/PickupsController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class PickupsController : MonoBehaviour
+{
+    public int pickupCount;
+    public GameObject pickupPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random rand = new Random();
+        
+        for (int i = 0; i < pickupCount; i++)
+        {
+            GameObject pickup = Instantiate(pickupPrefab, new Vector3(rand.Next(-11, 11), rand.Next(-11, 11), 0), Quaternion.identity);
+            pickup.transform.parent = transform;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public GameObject pickups;
+    public Text winText;
+    private Rigidbody2D body;
+    private int count;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        count = 0;
+        
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+        
+        body.AddForce(new Vector2(moveH, moveV) * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            
+            count++;
+            SetCountText();
+        }
+    }
+    
+    void SetCountText()
+    {
+        int pickupCount = pickups.GetComponent<PickupsController>().pickupCount;
+
+        countText.text = "Count: " + count + " / " + pickupCount;
+
+        if (count >= pickupCount)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
